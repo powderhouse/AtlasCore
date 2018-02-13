490839e2d0e20d7909e13ce82b33b5225d25b6e9
Jared Cosulich <jared.cosulich@gmail.com>
2018-02-13 14:43:40 -0500

    trying to expose git

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "AtlasCore",
-            targets: ["AtlasCore"]),
+            targets: ["AtlasCore", "Git"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
