70a73e65875c3522591eaf0a823d924ea2537c99
Jared Cosulich <jared.cosulich@gmail.com>
2018-05-16 20:15:32 -0400

    basic searching

--- Sources/AtlasCore/Search.swift
+++ Sources/AtlasCore/Search.swift
@@ -53,78 +53,9 @@ public class Search {
         
         return false
     }
-
-//    let kSearchMax = 1000
-//    func search(_ queryText: String) {
-//        var options: SKSearchOptions = kSKSearchOptionDefault
-////        if searchOptionNoRelevance != 0 {
-////            options |= kSKSearchOptionNoRelevanceScores
-////        }
-////        if searchOptionSpaceIsOR != 0 {
-////            options |= kSKSearchOptionSpaceMeansOR
-////        }
-////        if searchOptionSpaceFindSimilar != 0 {
-////            options |= kSKSearchOptionFindSimilar
-////        }
-//
-//        let query = NSString(string: queryText)
-//        var search = SKSearchCreate(skIndex, query, options).takeRetainedValue()
-//
-//        //..........................................................................
-//        // get matches from a search object
-//        var more = true
-//        var totalCount: UInt32 = 0
-//        while more {
-//            var foundDocIDs = [SKDocumentID](repeating: SKDocumentID(), count: kSearchMax)
-//            var foundScores = [Float](repeating: 0.0, count: kSearchMax)
-//            var foundDocRefs = [SKDocument?](repeating: nil, count: kSearchMax)
-//            var scores = [Float]()
-////            var unranked = Bool(options & kSKSearchOptionNoRelevanceScores)
-////            if unranked {
-////                scores = nil
-////            } else {
-////                scores = foundScores
-////            }
-//            scores = foundScores
-//
-//            var foundCount = CFIndex(0)
-//            var pos: CFIndex = 0
-//            more = SKSearchFindMatches(search, kSearchMax, foundDocIDs, scores, 1, foundCount)
-//            totalCount += foundCount
-//
-//            SSKIndexCopyDocumentRefsForDocumentIDs(mySKIndex as? SKIndex?, CFIndex(foundCount), foundDocIDs as? SKDocumentID, foundDocRefs as? SKDocument?)
-//
-//
-//            for pos in 0..<foundCount {
-//                var doc = foundDocRefs[pos] as? SKDocument?
-//                var url = SKDocumentCopyURL(doc)
-//                var urlStr = url.absoluteString
-//                var desc = ""
-//                if unranked {
-//                desc = "---\nDocID: \(Int(foundDocIDs[pos])), URL: \(urlStr)"
-//                } else {
-//                desc = "---\nDocID: \(Int(foundDocIDs[pos])), Score: \(foundScores[pos]), URL: \(urlStr)"
-//                }
-//                log(desc)
-//            }
-//
-//            var desc = "\"\(query)\" - \(Int(totalCount)) matches"
-//            log(desc)
-//        }
-    
     
     
     public func search(_ terms: String) -> [NSURL] {
-//        let stopwords: Set = ["all", "and", "its", "it's", "the"]
-//
-//        let properties: [NSObject: AnyObject] = [
-//            NSString("kSKStartTermChars"): "", // additional starting-characters for terms
-//            NSString("kSKTermChars"): "-_@.'", // additional characters within terms
-//            NSString("kSKEndTermChars"): "",   // additional ending-characters for terms
-//            NSString("kSKMinTermLength"): 3,
-//            NSString("kSKStopWords"): stopwords
-//        ]
-
         let query = NSString(string: terms)
         let options = SKSearchOptions(kSKSearchOptionDefault)
         let search = SKSearchCreate(skIndex, query, options).takeUnretainedValue()
@@ -137,19 +68,24 @@ public class Search {
         var scores: [Float] = Array(repeating: 0, count: limit)
         var count: CFIndex = 0
 
-        let hasMoreResults = SKSearchFindMatches(search, limit, &documentIDs, &scores, time, &count)
+        var hasMoreResults = true
+        var allResults: [NSURL] = []
+        while hasMoreResults {
+            hasMoreResults = SKSearchFindMatches(search, limit, &documentIDs, &scores, time, &count)
+            
+            SKIndexCopyDocumentURLsForDocumentIDs(skIndex, count, &documentIDs, &urls)
+            
+            let results: [NSURL] = zip(urls[0 ..< count], scores).flatMap({
+                (cfurl, score) -> NSURL? in
+                guard let url = cfurl?.takeUnretainedValue() as NSURL?
+                    else { return nil }
+                
+                return url
+            })
+            allResults += results
+        }
 
-        SKIndexCopyDocumentURLsForDocumentIDs(skIndex, count, &documentIDs, &urls)
-
-        let results: [NSURL] = zip(urls[0 ..< count], scores).flatMap({
-            (cfurl, score) -> NSURL? in
-            guard let url = cfurl?.takeUnretainedValue() as NSURL?
-                else { return nil }
-
-            return url
-        })
-
-        return results
+        return allResults
     }
     
     public func close() {
