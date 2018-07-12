212086a4338618c6c5818d45093162a409640420
Jared Cosulich <jared.cosulich@gmail.com>
2018-07-12 13:55:51 -0400

    testing git move

--- Sources/AtlasCore/AtlasCore.swift
+++ Sources/AtlasCore/AtlasCore.swift
@@ -15,7 +15,7 @@ public struct Commit {
 
 public class AtlasCore {
     
-    public static let version = "1.0.9"
+    public static let version = "1.1.0"
     public static let defaultProjectName = "General"
 
     public let appName = "Atlas"
