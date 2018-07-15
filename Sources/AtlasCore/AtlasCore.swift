f41ebae7730ee2bfdcb4276483d16d4593f2eb96
Jared Cosulich <jared.cosulich@gmail.com>
2018-07-14 21:44:22 -0400

    update version

--- Sources/AtlasCore/AtlasCore.swift
+++ Sources/AtlasCore/AtlasCore.swift
@@ -15,7 +15,7 @@ public struct Commit {
 
 public class AtlasCore {
     
-    public static let version = "1.1.5"
+    public static let version = "1.1.7"
     public static let defaultProjectName = "General"
 
     public static let appName = "Atlas"
