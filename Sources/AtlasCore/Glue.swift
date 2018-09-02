b8528951c6d17888d9ed88d3373a3b9358f4e48c
Jared Cosulich <jared.cosulich@gmail.com>
2018-09-02 14:25:27 -0400

    working on bundle integration

--- Sources/AtlasCore/Glue.swift
+++ Sources/AtlasCore/Glue.swift
@@ -11,28 +11,42 @@ public class Glue {
     
     public static let path: String = "$PATH:/bin:/usr/bin:/usr/local/bin:/anaconda/bin"
     
-    public class func runProcess(_ command: String, arguments: [String]?=[], environment_variables: [String: String]?=nil, currentDirectory: URL?=nil, atlasProcess: AtlasProcess=Process()) -> String {
-        var process = atlasProcess
+    class func gitDir() -> String? {
+        return Bundle(for: AtlasCore.self).resourcePath?.appending("/git/bin/")
+    }
+    
+    class func addEnvVars(to existing: [String: String]?) -> [String: String] {
+        var env = existing ?? [:]
         
-        process.launchPath = "/usr/bin/env"
-//        process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
-        process.arguments = [command] + (arguments ?? [])
+        env["HOME"] = NSSearchPathForDirectoriesInDomains(
+            .documentDirectory,
+            .userDomainMask,
+            true
+            )[0]
         
-        var environment = (environment_variables ?? [:])
-        environment["PATH"] = path
-
-        if currentDirectory != nil {
-            //            process.currentDirectoryURL = currentDirectory
-            process.currentDirectoryPath = currentDirectory!.path
-            environment["HOME"] = currentDirectory!.path
+        env["PATH"] = path
+        if let gitDir = gitDir() {
+            env["PATH"]?.append(":\(gitDir)")
         }
+
+        env["GIT_CONFIG_NOSYSTEM"] = "1"
         
-        process.environment = environment
-        
+        return env
+    }
+    
+    public class func runProcess(_ command: String, arguments: [String]?=[], environment_variables: [String: String]?=nil, currentDirectory: URL?=nil, atlasProcess: AtlasProcess=Process()) -> String {
+        let process = initializeProcess(command, arguments: arguments, environment_variables: environment_variables, currentDirectory: currentDirectory, atlasProcess: atlasProcess)
+
         return process.runAndWait()
     }
     
     public class func runProcessError(_ command: String, arguments: [String]?=[], environment_variables: [String: String]?=nil, currentDirectory: URL?=nil, atlasProcess: AtlasProcess=Process()) -> String {
+        let process = initializeProcess(command, arguments: arguments, environment_variables: environment_variables, currentDirectory: currentDirectory, atlasProcess: atlasProcess)
+        
+        return process.runAndWaitError()
+    }
+    
+    class func initializeProcess(_ command: String, arguments: [String]?, environment_variables: [String: String]?, currentDirectory: URL?, atlasProcess: AtlasProcess) -> AtlasProcess {
         var process = atlasProcess
         
         process.launchPath = "/usr/bin/env"
@@ -40,15 +54,11 @@ public class Glue {
         process.arguments = [command] + (arguments ?? [])
         
         if currentDirectory != nil {
-            //            process.currentDirectoryURL = currentDirectory
             process.currentDirectoryPath = currentDirectory!.path
         }
         
-        var environment = (environment_variables ?? [:])
-        environment["PATH"] = path
-        process.environment = environment
-        
-        return process.runAndWaitError()
+        process.environment = addEnvVars(to: environment_variables)
+        return process
     }
     
 }
