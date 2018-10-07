8cc4b9c28538e5a18a05d53b9d22b39c677a989c
atlastest <jared.cosulich@gmail.com>
2018-10-07 11:18:25 -0400

    using latest asw-swift-sdk-core

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/swift-aws/iam.git", from: "1.0.0"),
+        .package(url: "https://github.com/powderhouse/iam.git", .exact("0.3.4")),
         .package(url: "https://github.com/Quick/Quick.git", from: "1.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", from: "7.1.0"),
         ],
